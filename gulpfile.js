c18af8e2e066a0139a4d472ea2d5ab72c47c1054
Robert Sebescen <pgrobban@gmail.com>
2016-09-10 21:31:24 +0200

    recommendation box fixes and now have sass

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,13 @@ var browserify = require("browserify");
 var babelify = require("babelify");
 var source = require('vinyl-source-stream');
 var gutil = require('gulp-util');
+var sass = require('gulp-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./compiled-app'));
+});
 
 // Lets bring es6 to es5 with this.
 // Babel - converts ES6 code to ES5 - however it doesn't handle imports.
@@ -21,12 +28,13 @@ gulp.task('es6', function() {
         })
         .bundle()
         .on('error', gutil.log)
-        .pipe(source('compiled-js/app.js'))
+        .pipe(source('compiled-app/app.js'))
         .pipe(gulp.dest('./'));
 });
 
 gulp.task('watch', function() {
     gulp.watch(['./app/**/*.js', './data/**/*.json'], ['es6']);
+    gulp.watch(['./sass/**/*.scss'], ['sass']);
 });
 
-gulp.task('default', ['es6', 'watch']);
+gulp.task('default', ['sass', 'es6', 'watch']);
