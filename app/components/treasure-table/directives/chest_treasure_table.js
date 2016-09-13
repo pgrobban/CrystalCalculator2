ce4f256495bd94c41b6f1f019c5b358b06850f20
Robert Sebescen <pgrobban@gmail.com>
2016-09-13 22:54:51 +0200

    testing increased timeout

--- app/components/treasure-table/directives/chest_treasure_table.js
+++ app/components/treasure-table/directives/chest_treasure_table.js
@@ -24,7 +24,7 @@ class ChestTreasureTableController {
         }
       });
     }
-    $timeout(this.updateSaveState.bind(this), 50);
+    $timeout(this.updateSaveState.bind(this), 200);
   }
 
   updateSaveState() {
